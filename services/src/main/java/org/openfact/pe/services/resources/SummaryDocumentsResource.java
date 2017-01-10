9378d99379bcdd5c34d5d0811c4783b1e48f0d17
Alex Pariona <lx7pary@gmail.com>
2017-01-10 09:06:40 -0500

    update senser utils

--- services/src/main/java/org/openfact/pe/services/resources/SummaryDocumentsResource.java
+++ services/src/main/java/org/openfact/pe/services/resources/SummaryDocumentsResource.java
@@ -40,6 +40,7 @@ import org.openfact.models.search.SearchCriteriaModel;
 import org.openfact.models.search.SearchResultsModel;
 import org.openfact.models.utils.ModelToRepresentation;
 import org.openfact.models.utils.RepresentationToModel;
+import org.openfact.pe.constants.EmissionType;
 import org.openfact.pe.models.SummaryDocumentModel;
 import org.openfact.pe.models.SummaryDocumentProvider;
 import org.openfact.pe.models.types.summary.SummaryDocumentsType;
@@ -361,7 +362,7 @@ public class SummaryDocumentsResource {
 			throw new NotFoundException("Ticket not found");
 		}
 		if (sendEvent.getFileResponseAttatchments().isEmpty()) {
-			byte[] result = new SunatSenderUtils(organization).getStatus(ticket);
+			byte[] result = new SunatSenderUtils(organization, EmissionType.CPE).getStatus(ticket);
 			if (result == null) {
 				throw new NotFoundException("Sunat response, cdr not found");
 			}
